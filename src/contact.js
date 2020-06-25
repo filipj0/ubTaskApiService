2e1b7d3910ec2841db25fada0ba85a4c4f27e90c
INTRANET\fjakovljevic <fjakovljevic@cs.hr>
2020-06-25 11:02:25 +0200

    Code cleanup

--- src/contact.js
+++ src/contact.js
@@ -23,9 +23,4 @@ createRouter = () => {
     return router;
 };
 
-validateEmail = (email) => {
-    let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailPattern.test(email);
-}
-
 module.exports = createRouter;
